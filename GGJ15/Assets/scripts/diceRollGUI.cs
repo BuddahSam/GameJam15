5ce2f5ab1ca389186c642a16009629387f2c4d3c
mcnultor <ryanmcnulty3570@gmail.com>
2015-01-24 18:43:35 +0000

    start for combat

---
+++ GGJ15/Assets/scripts/diceRollGUI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class diceRollGUI : MonoBehaviour {
+
+	public Texture2D backGroundTexture;
+	public GUISkin skin;
+	
+	void OnGUI()
+	{
+				GUI.skin = skin;
+		
+				GUI.DrawTexture (new
+		                 Rect (0, 0, 175, 250), backGroundTexture);
+
+		}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
