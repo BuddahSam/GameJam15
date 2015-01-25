cc04354a2426530197f12e086cb3e8acf5c5bd8e
BuddahSam <buddahsam@gmail.com>
2015-01-25 13:58:33 +0000

    stuff

---
+++ GGJ15/Assets/scripts/diceRollScripts/battleRoll/bearBattle/bearDeffenceGUI.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bearDeffenceGUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
